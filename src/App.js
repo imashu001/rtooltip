1ba62ff74e7567dafa0a8f44918978b754e9bf75
imashu001 <60100740+imashu001@users.noreply.github.com>
2020-10-16 23:56:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,57 @@
 import React from "react";
 import "./App.css";
-import ReactTooltip from "react-tooltip";
+import Tooltip from "./Tooltip";
 
-function App() {
-  return (
-    <div className="App">
-      <ReactTooltip id="tooltip" place="top" effect="solid">
-        Tooltip
-      </ReactTooltip>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showTtip: false,
+      position: "top",
+    };
+  }
+  togglettip = (e) => {
+    if (this.state.position === "") {
+      this.setState({
+        position: "top",
+      });
+    }
+    this.setState({
+      showTtip: !this.state.showTtip,
+    });
+  };
+  handleChange = (e) => {
+    const a = e.target.value;
+    console.log(a);
+    if (a !== null) {
+      this.setState({
+        position: a,
+      });
+    }
+  };
+  render() {
+    return (
+      <div className="App">
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.position}
+          placeholder="Position"
+        />
+        <br />
+        <br />
+        <p> Current position of tooltip is {this.state.position}</p>
+        <div
+          className="ttip"
+          onMouseOver={this.togglettip}
+          onMouseOut={this.togglettip}
+        >
+          Hover Over
+          {this.state.showTtip && <Tooltip position={this.state.position} />}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
